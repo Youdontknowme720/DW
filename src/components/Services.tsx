7f5b5cbce52f0703e41512c61e21e47fe3259e18
ayanokouji <wenzelmannnico1@gmail.com>
2026-01-22 07:29:03 +0100

    + WIP

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,12 +3,12 @@ import '../styles/Services.css';
 import reactLogo from '../assets/react.svg';
 
 const serviceCards = [
-    { img: reactLogo, description: "Individuelle Lösungen" },
-    { img: reactLogo, description: "Qualität" },
-    { img: reactLogo, description: "Termintreue Lieferungen" },
-    { img: reactLogo, description: "Schneller Service" },
-    { img: reactLogo, description: "Support rund um die Uhr" },
-    { img: reactLogo, description: "Nachhaltigkeit" },
+    { img: reactLogo, description: "Vom Designschild bis hin zum Industrieschild bieten wir Schilder für fast alle Einsatzzwecke und aus den unterschiedlichsten Materialien." },
+    { img: reactLogo, description: "Im 2 ½ D-Bereich fräsen wir Teile aus Aluminium, Kunststoff und Messing." },
+    { img: reactLogo, description: "Vom Designschild bis hin zum Industrieschild bieten wir Schilder für fast alle Einsatzzwecke und aus den unterschiedlichsten Materialien." },
+    { img: reactLogo, description: "Vom Designschild bis hin zum Industrieschild bieten wir Schilder für fast alle Einsatzzwecke und aus den unterschiedlichsten Materialien." },
+    { img: reactLogo, description: "Vom Designschild bis hin zum Industrieschild bieten wir Schilder für fast alle Einsatzzwecke und aus den unterschiedlichsten Materialien." },
+    { img: reactLogo, description: "Vom Designschild bis hin zum Industrieschild bieten wir Schilder für fast alle Einsatzzwecke und aus den unterschiedlichsten Materialien." },
 ];
 
 export default function Services() {
