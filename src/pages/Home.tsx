68a3e56515c328fc413ea3d870f4a54b0b9e6d85
ayanokouji <wenzelmannnico1@gmail.com>
2026-02-17 17:49:23 +0100

    text back

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,17 @@ export default function Home() {
       <div className="service-wrapper">
         <Hero
           title={<>Willkommen bei der <br /><span style={{ color: "blue" }}>Dieter Wenzelmann GmbH</span><br />in 57648 Unnau/Westerwald</>}
-          description="..."
+          description="Wir präsentieren Ihnen auf den nachfolgenden Seiten unser breites Spektrum an Schildern und Aluminium-Frästeilen. 
+          Von einfachen Industrieschildern aus Kunststoff, Aluminium oder Schildern aus Edelstahl in 
+          gravierter oder geätzter Ausführung, über den Digitaldruck und Eloxaldruck bis hin zur Laserbeschriftung bieten wir alles genau nach Kundenwunsch an. 
+          Dies gilt natürlich auch für Frontplatten und Bedienpanels, die wir nach Ihrer Zeichnung oder Datei individuell anfertigen.
+          Wir fräsen für Sie Aluminium, und vieles mehr. Mit unseren modernen, großflächigen CNC-Graviermaschinen und CNC-Fräsmaschinen
+          stellen wir für Sie 2 ½D Frästeile aus Aluminium, Kunststoffen und Messing in 1A-Qualität her.
+          Dank unseres großen Maschinenparks produzieren wir kostengünstig und gewährleisten kurze Lieferzeiten.
+          So können wir und auch Sie „just in time“ beliefern. Lassen Sie sich von unserer Internetpräsenz anregen
+          oder nehmen Sie gleich persönlich mit uns Kontakt auf.
+          Wir beraten Sie kompetent und erstellen Ihnen gerne ein individuelles Angebot.
+          Ihre Dieter Wenzelmann GmbH – Industrieschilder – Aluminiumbearbeitung"
           image={rlogo}
         />
       </div>
