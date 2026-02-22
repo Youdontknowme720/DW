765bc02de18f9fdc6d0a57dc752af0d4dfe69064
ayanokouji <wenzelmannnico1@gmail.com>
2026-02-22 20:31:24 +0100

    scroll to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import ScrollToTop from "./components/ScrollTo";
 import Home from "./pages/Home";
 import Impressum from "./pages/Impressum";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
