281dfb745cbc6b3c55e8c2f8e79e7a07378169b3
ayanokouji <wenzelmannnico1@gmail.com>
2026-03-06 20:13:38 +0100

    Fixed promise card spacing

--- src/components/PromiseCard.tsx
+++ src/components/PromiseCard.tsx
@@ -10,8 +10,8 @@ function PromiseCard({icon, heading, text }: PromiseCard) {
     return (
         <div className="p-card">
             <div className="card-icon">{icon}</div>
-            <h2 className="card-heading" style={{gridArea: "box-heading"}}>{heading}</h2>
-            <p className="card-text" style={{gridArea: "box-text"}}>{text}</p>
+            <h2 className="card-heading">{heading}</h2>
+            <p className="card-text">{text}</p>
         </div>
     )
 }
